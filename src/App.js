bd344159c4ebc810441650aca993d254f2505fd4
visualtechnet <mark2579@gmail.com>
2020-11-22 07:37:59 -0500

    added final fetch, just logging for now

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import './App.css';
 
 const searchTweetTerm = (searchTerm) => {
   const { REACT_APP_APIURL, REACT_APP_APIBEARER, REACT_APP_MAXRESULT } = process.env
-  const fullUrl = `${REACT_APP_APIURL}?q="${searchTerm}"&result_type=popular&count=${REACT_APP_MAXRESULT}`
+  const fullUrl = `${REACT_APP_APIURL}/1.1/search/tweets.json?q="${searchTerm}"&result_type=popular&count=${REACT_APP_MAXRESULT}`
 
   return fetch(fullUrl, {
     method: 'GET',
+    mode: 'no-cors',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `bearer ${REACT_APP_APIURL}`
+      'Authorization': `bearer ${REACT_APP_APIBEARER}`
     }
-  }).then(d => d.json())
+  }).then(d => d.json())  
 }
 
 
@@ -26,7 +27,8 @@ function App() {
   const onSearchTweet = async (e) => {
     if(searchTerm.length > 0) {
       //invoke api
-      //await searchTweetTerm(searchTerm)
+      const tweetResult = await searchTweetTerm(searchTerm).then((result) => result)
+      console.log(tweetResult)
     }
 
     e.preventDefault();
